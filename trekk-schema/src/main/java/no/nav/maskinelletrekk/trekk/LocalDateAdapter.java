a6014325b4f1931615cc19b4d0afe4dbeee8cdae
Magnar Brandsdal <magnar.brandsdal@nav.no>
2018-04-06 10:52:45 +0200

    PK-52404 Endring i schema. TrekkvedtakId er int. Endret parser til å bruke ISO_DATE.

--- trekk-schema/src/main/java/no/nav/maskinelletrekk/trekk/LocalDateAdapter.java
+++ trekk-schema/src/main/java/no/nav/maskinelletrekk/trekk/LocalDateAdapter.java
@@ -13,9 +13,9 @@ public class LocalDateAdapter extends XmlAdapter<String,LocalDate> {
     private static final Logger LOGGER = LoggerFactory.getLogger(LocalDateAdapter.class);
 
     @Override
-    public LocalDate unmarshal(String localDateAsString) throws Exception {
+    public LocalDate unmarshal(String localDateAsString) {
         try {
-            return LocalDate.parse(localDateAsString, DateTimeFormatter.ISO_DATE_TIME);
+            return LocalDate.parse(localDateAsString, DateTimeFormatter.ISO_DATE);
         } catch (DateTimeParseException e) {
             LOGGER.error("Parsing av dato '{}' feilet!", localDateAsString);
             return null;
@@ -23,7 +23,7 @@ public class LocalDateAdapter extends XmlAdapter<String,LocalDate> {
     }
 
     @Override
-    public String marshal(LocalDate localDate) throws Exception {
-        return localDate != null ? localDate.format(DateTimeFormatter.ISO_DATE_TIME) : null;
+    public String marshal(LocalDate localDate) {
+        return localDate != null ? localDate.format(DateTimeFormatter.ISO_DATE) : null;
     }
 }

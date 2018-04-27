d80e35ae416e2e21114a5862d11976d49cf6abce
Magnar Brandsdal <magnar.brandsdal@nav.no>
2018-04-27 13:22:21 +0200

    PK-52404 Fjernet privat konstruktør

--- trekk-app/src/main/java/no/nav/maskinelletrekk/trekk/Bootstrap.java
+++ trekk-app/src/main/java/no/nav/maskinelletrekk/trekk/Bootstrap.java
@@ -4,10 +4,8 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@SuppressWarnings("squid:S1118")
 public class Bootstrap {
-
-    private Bootstrap() {
-    }
 
     public static void main(String[] args) {
         SpringApplication.run(Bootstrap.class, args);

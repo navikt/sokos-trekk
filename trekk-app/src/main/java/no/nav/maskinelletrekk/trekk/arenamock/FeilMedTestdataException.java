ee8c922f7ec7a052d387d3a15ed3a8374e566df4
Magnar Brandsdal <magnar.brandsdal@nav.no>
2018-08-09 09:35:31 +0200

    PK-51245 Fjerner foreløpig mock koden

--- trekk-app/src/main/java/no/nav/maskinelletrekk/trekk/arenamock/FeilMedTestdataException.java
+++
@@ -1,9 +0,0 @@
-package no.nav.maskinelletrekk.trekk.arenamock;
-
-class FeilMedTestdataException extends Exception {
-    private static final long serialVersionUID = -3068247030192939857L;
-
-    FeilMedTestdataException(String message, Throwable cause) {
-        super(message, cause);
-    }
-}

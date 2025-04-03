307f04ddd13465ec9da9198e45ecd120ef368341
takwaiwang <tak.wai.wang@nav.no>
2025-04-03 13:25:31 +0200

    Fjern uønsket logging

---
+++ src/main/kotlin/no/nav/sokos/trekk/config/ShibbolethWarningFilter.kt
@@ -0,0 +1,17 @@
+package no.nav.sokos.trekk.config
+
+import ch.qos.logback.classic.spi.ILoggingEvent
+import ch.qos.logback.core.boolex.EvaluationException
+import ch.qos.logback.core.boolex.EventEvaluatorBase
+
+/**
+ * Use this to filter unwanted log messages.
+ */
+class ShibbolethWarningFilter : EventEvaluatorBase<ILoggingEvent>() {
+    @Throws(NullPointerException::class, EvaluationException::class)
+    override fun evaluate(event: ILoggingEvent): Boolean {
+        val message = event.message
+        return message.contains("net.shibboleth.utilities.java.support.primitive.StringSupport") &&
+            message.contains("This will be removed in the next major version")
+    }
+}

27938d8cfe1216e8e880b40c7f749c99926a0bef
mudasar187 <mudasar_cheema@hotmail.com>
2025-05-27 09:32:42 +0200

    fjerne unødvendig logger

--- src/main/kotlin/no/nav/sokos/trekk/api/TrekkApi.kt
+++ src/main/kotlin/no/nav/sokos/trekk/api/TrekkApi.kt
@@ -9,18 +9,13 @@ import io.ktor.server.response.respondText
 import io.ktor.server.routing.Route
 import io.ktor.server.routing.post
 import io.ktor.server.routing.route
-import mu.KotlinLogging
 
 import no.nav.sokos.trekk.service.BehandleTrekkvedtakService
 import no.nav.sokos.trekk.util.JaxbUtils
 
-private val logger = KotlinLogging.logger {}
-
 fun Route.trekkApi(behandleTrekkvedtakService: BehandleTrekkvedtakService = BehandleTrekkvedtakService()) {
     route("/api/trekk") {
         post("/behandle") {
-            logger.info { "Behandle trekk data." }
-
             val xmlContent = call.receiveText()
             val trekk =
                 behandleTrekkvedtakService.behandleTrekkvedtak(

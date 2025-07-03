c4d5f07bb4bb15fc7b9ec9cc77c6c3ba092ac8e4
mudasar187 <mudasar_cheema@hotmail.com>
2025-07-03 11:28:46 +0200

    endre johnrengelman plugin til com.gradleup.com

--- build.gradle.kts
+++ build.gradle.kts
@@ -9,7 +9,7 @@ import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 plugins {
     kotlin("jvm") version "2.2.0"
     kotlin("plugin.serialization") version "2.2.0"
-    id("com.github.johnrengelman.shadow") version "8.1.1"
+    id("com.gradleup.shadow") version "8.3.8"
     id("org.jlleitschuh.gradle.ktlint") version "12.3.0"
     id("org.jetbrains.kotlinx.kover") version "0.9.1"
 }

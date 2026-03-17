f42b7822909f15308d4238b7dc8c3b40e199538b
mudasar187 <mudasar_cheema@hotmail.com>
2026-03-17 14:22:03 +0100

    chore: lukke sårbarheter for com.fasterxml.jackson, tools.jackson.core

--- build.gradle.kts
+++ build.gradle.kts
@@ -121,6 +121,21 @@ dependencies {
     testImplementation("io.mockk:mockk:$mockkVersion")
 }
 
+configurations.all {
+    resolutionStrategy {
+        eachDependency {
+            if (requested.group == "tools.jackson.core" && requested.name == "jackson-core") {
+                useVersion("3.1.0")
+                because("jackson-core has Nesting Depth Constraint Bypass in `UTF8DataInputJsonParser` potentially allowing Resource Exhaustion. Affected version >= 3.0.0, < 3.1.0")
+            }
+            if (requested.group == "com.fasterxml.jackson.core" && requested.name == "jackson-core") {
+                useVersion("2.21.1")
+                because("jackson-core: Number Length Constraint Bypass in Async Parser Leads to Potential DoS Condition. Affected version >= 2.19.0, < 2.21.1")
+            }
+        }
+    }
+}
+
 sourceSets {
     main {
         java {
